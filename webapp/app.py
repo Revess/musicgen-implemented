5608e1fe3ae7134931281ac27f3fe91c7dab4f6e
Revess <bjmaat@gmail.com>
2024-08-22 10:31:13 +0000

    working

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, jsonify, send_from_directory, render_template
-from transformers import pipeline
-import scipy
-import os
-import uuid
+from flask import Flask, request, jsonify, send_from_directory, render_template, abort
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+import os, torchaudio, uuid, glob, time, shutil
+from apscheduler.schedulers.background import BackgroundScheduler
 
-synthesiser = pipeline("text-to-audio", "facebook/musicgen-small", device="cpu")
+simple_debug = False
+
+if not simple_debug:
+    model_prompt = MusicGen.get_pretrained("facebook/musicgen-stereo-small", device='cpu')
+    model_melody = MusicGen.get_pretrained("facebook/musicgen-stereo-melody", device='cpu')
+    print('Done loading models')
 
 # Initialize the Flask application
 app = Flask(__name__, static_url_path='/static')
@@ -13,36 +18,95 @@ app = Flask(__name__, static_url_path='/static')
 AUDIO_FOLDER = os.path.join(os.getcwd(), 'static', 'audio')
 os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
+# Function to delete folders not modified in the last 24 hours
+def clean_old_folders():
+    current_time = time.time()
+    for folder_name in os.listdir(AUDIO_FOLDER):
+        folder_path = os.path.join(AUDIO_FOLDER, folder_name)
+        if os.path.isdir(folder_path):
+            last_modified_time = os.path.getmtime(folder_path)
+            # If the folder is older than 24 hours (86400 seconds), delete it
+            if current_time - last_modified_time > 86400: 
+                shutil.rmtree(folder_path)
+                print(f"Deleted folder: {folder_path}")
+
+# Schedule the clean_old_folders function to run every day
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=clean_old_folders, trigger="interval", days=1)
+scheduler.start()
+
 # Replace this function with your actual audio generation logic
-def generate_audio(prompt):
-    music = synthesiser(prompt, forward_params={"do_sample": True})
+def generate_audio(prompt, temperature, topk, topp, cfg, samples, duration, dropped=False, userid=None):
+    if simple_debug:
+        print('loading prompt')
+        print(prompt, temperature, topk, topp, cfg, samples, duration)
+        print([f"{AUDIO_FOLDER}/{userid}/8b83f79f-80e9-4b5a-b62a-0d4124d1c809.wav"] * samples)
+        return [f"{AUDIO_FOLDER}/{userid}/8b83f79f-80e9-4b5a-b62a-0d4124d1c809.wav"] * samples
+    
+    model_prompt.set_generation_params(
+        duration=duration,
+        temperature=temperature,
+        top_k=topk,
+        top_p=topp,
+        cfg_coef=cfg
+    )
 
-    audio_file_path = os.path.join(AUDIO_FOLDER, f"{uuid.uuid4()}.wav")
-    scipy.io.wavfile.write(audio_file_path, rate=music["sampling_rate"], data=music["audio"])
+    audio_file_paths = [os.path.join(AUDIO_FOLDER, userid, f"{uuid.uuid4()}") for _ in range(samples)]
+    if dropped:
+        melody, sr = torchaudio.load(dropped)
+        for audio_file_path in audio_file_paths:
+            wav = model_melody.generate_with_chroma([prompt], melody[None].expand(1, -1, -1), sr)
+            audio_write(audio_file_path, wav[0].cpu(), model_prompt.sample_rate, strategy="loudness")
+    else:
+        for audio_file_path in audio_file_paths:
+            wav = model_prompt.generate([prompt])
+            audio_write(audio_file_path, wav[0].cpu(), model_prompt.sample_rate, strategy="loudness")
 
-    return audio_file_path
+    return [f"{audio_file_path}.wav" for audio_file_path in audio_file_paths ]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        prompt = request.form.get('prompt')
+        prompt = request.form.get('prompt').lower()
+        userid = request.form.get('userid')
+        if userid == 'undefined' or userid not in os.listdir(AUDIO_FOLDER):
+            userid = uuid.uuid4()
+            os.makedirs(f"{AUDIO_FOLDER}/{userid}", exist_ok=True)
+        else:
+            for file_ in glob.glob(f"{AUDIO_FOLDER}/{userid}/*"):
+                os.unlink(file_)
+        # Handle the uploaded audio file
+        audio_file = request.files.get('audioInput')
+        dropped = False
+        if audio_file and audio_file.filename != '':
+            dropped = os.path.join(AUDIO_FOLDER, userid, f"{uuid.uuid4()}.wav")
+            audio_file.save(dropped)
         if prompt:
             try:
-                audio_file_path = generate_audio(prompt)
-                audio_filename = os.path.basename(audio_file_path)
-                download_link = f'/download/{audio_filename}'
-                return jsonify({'success': True, 'download_link': download_link})
+                temperature = float(request.form.get('Temperature'))
+                topk = int(request.form.get('Top K'))
+                topp = float(request.form.get('Top P'))
+                cfg = int(request.form.get('Classifier Free Guidance'))
+                samples = int(request.form.get('Samples'))
+                duration = int(request.form.get('Duration'))
+                audio_file_paths = generate_audio(prompt, temperature, topk, topp, cfg, samples, duration, dropped=dropped, userid=userid)
+                audio_filenames = [os.path.basename(audio_file_path) for audio_file_path in audio_file_paths]
+                download_links = [f'/download/{userid}/{audio_filename}' for audio_filename in audio_filenames]
+                return jsonify({'success': True, 'download_links': download_links, 'userid': userid})
             except Exception as e:
                 return jsonify({'success': False, 'error': str(e)}), 500
     return render_template('index.html', download_link=None)
 
-@app.route('/download/<filename>')
-def download(filename):
-    return send_from_directory(AUDIO_FOLDER, filename, as_attachment=True)\
+@app.route('/download/<userid>/<filename>')
+def download(userid, filename):
+    return send_from_directory(os.path.join(AUDIO_FOLDER, userid), filename, as_attachment=True)
     
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+    try:
+        app.run(debug=True, port=5500)
+    finally:
+        scheduler.shutdown()
